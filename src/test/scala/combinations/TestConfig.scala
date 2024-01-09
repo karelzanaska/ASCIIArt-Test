2e95e2d96e255334ba646a54439cfe162de52710
zanaskar <zanaskar@fit.cvut.cz>
2024-01-09 17:04:13 +0100

    fix paths

--- src/test/scala/combinations/TestConfig.scala
+++ src/test/scala/combinations/TestConfig.scala
@@ -9,13 +9,22 @@ case class FloatValue(value: Float) extends ConfigValue
 case class NullValue() extends ConfigValue
 
 
+//val base= "samples/"
+//val basePath = "sam"
+val baseSamplePath = "samples/"
 
 object TestConfig {
   val importers: Map[String, ConfigValue] = Map(
     "asciiart.image.importers.JPGImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.jpg"),
-    "asciiart.image.importers.PNGImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.png"),
-    "asciiart.image.importers.BMPImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.bmp"),
-    "asciiart.image.importers.GIFImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.gif"),
+    "asciiart.image.importers.JPGImageImporter" -> StringValue(baseSamplePath + "cloud.jpg"),
+
+//    "asciiart.image.importers.PNGImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.png"),
+    "asciiart.image.importers.PNGImageImporter" -> StringValue(baseSamplePath + "cloud.png"),
+//    "asciiart.image.importers.BMPImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.bmp"),
+    "asciiart.image.importers.BMPImageImporter" -> StringValue(baseSamplePath + "cloud.bmp"),
+//    "asciiart.image.importers.BMPImageImporter" -> StringValue(baseSamplePath + "cloud.bmp"),
+//    "asciiart.image.importers.GIFImageImporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/cloud.gif"),
+    "asciiart.image.importers.GIFImageImporter" -> StringValue(baseSamplePath + "cloud.gif"),
     "asciiart.image.importers.RandomImageGeneratorImporter" -> NullValue(),
     "asciiart.image.importers.GradientRandomImageGeneratorImporter" -> NullValue(),
   )
@@ -33,13 +42,18 @@ object TestConfig {
 
   val exporters: Map[String, ConfigValue] = Map(
     "asciiart.image.exporters.StdoutImageExporter" -> NullValue(),
-    "asciiart.image.exporters.TxtImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.txt"),
+//    "asciiart.image.exporters.TxtImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.txt"),
+    "asciiart.image.exporters.TxtImageExporter" -> StringValue(baseSamplePath + "output-cloud.txt"),
   )
 
   val rgbExporters: Map[String, ConfigValue] = Map(
-    "asciiart.image.exporters.JPGImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.jpg"),
-    "asciiart.image.exporters.PNGImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.png"),
-    "asciiart.image.exporters.BMPImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.bmp"),
-    "asciiart.image.exporters.GIFImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.gif"),
+//    "asciiart.image.exporters.JPGImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.jpg"),
+    "asciiart.image.exporters.JPGImageExporter" -> StringValue(baseSamplePath + "output-cloud.jpg"),
+//    "asciiart.image.exporters.PNGImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.png"),
+    "asciiart.image.exporters.PNGImageExporter" -> StringValue(baseSamplePath + "output-cloud.png"),
+//    "asciiart.image.exporters.BMPImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.bmp"),
+    "asciiart.image.exporters.BMPImageExporter" -> StringValue(baseSamplePath + "output-cloud.bmp"),
+//    "asciiart.image.exporters.GIFImageExporter" -> StringValue("/home/karel/FEL/ZKS/asciiart/samples/output-cloud.gif"),
+    "asciiart.image.exporters.GIFImageExporter" -> StringValue(baseSamplePath + "output-cloud.gif"),
   )
 }
